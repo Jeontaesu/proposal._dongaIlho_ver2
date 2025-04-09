6983ee3f8fce02f5e815807d8e94fb619c9ae83e
jeontaesu1 <jts@frameout.co.kr>
2025-04-09 15:10:51 +0900

    section4 수정

--- js/main.js
+++ js/main.js
@@ -1,29 +1,34 @@
 /**
  * @GSAP
  */
-import {markers} from './markers.js';
+import { markers } from "./markers.js";
 
-const sections = gsap.utils.toArray('.sc__wrapper .section');
+const sections = gsap.utils.toArray(".sc__wrapper .section");
 
 sections.forEach((item, index) => {
   ScrollTrigger.create({
     trigger: item,
-    scroller: '.sc',
-    start: 'top top',
-    end: index === sections.length - 1 ? '+=0%' : '+=100%',
-    pin:true,
-    pinSpacing:false, 
+    scroller: ".sc",
+    start: "top top",
+    end: index === sections.length - 1 ? "+=0%" : "+=100%",
+    // end:
+    //   index === sections.length - 1
+    //     ? "+=0%"
+    //     : index === sections.length - 2
+    //     ? "+=80%" // 여기 원하는 값으로
+    //     : "+=100%",
+    pin: true,
+    pinSpacing: false,
     // snap: {
     //   // snapTo: true,
     //   duration:0.3,
     //   ease: "power1.inOut"
     // },
-    // markers:true,
-  })
-})
+    markers: true,
+  });
+});
 
 // const wrapper = document.querySelector('.sc__wrapper')
 // wrapper.computedStyleMap.height = `${window.innerHeight + sections.length}px`;
 
-markers()
-
+markers();
